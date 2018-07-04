630efc5cfca47d3db0f39b2cff2c912875a26c90
emanuel.chiriac <emanuel.chiriac@P6611.svdomain1.softvision.ro>
2018-07-04 13:22:47 +0300

    Added delete

--- src/main/java/com/spring/restwsclient/PatientWSClient.java
+++ src/main/java/com/spring/restwsclient/PatientWSClient.java
@@ -25,14 +25,16 @@ public class PatientWSClient {
 		Patient patient = getPatient(100L);
 
 		patient.setName("Dragnea loves jilava");
-		
+
 		updatePatient(patient);
 
 		getPatient(100L);
 
-		insertPatient(new Patient("Viorica"));
+		patient = insertPatient(new Patient("Viorica"));
 
 		getAllPatients();
+
+		deletePatient(patient.getId());
 	}
 
 	//Update patient
@@ -42,15 +44,29 @@ public class PatientWSClient {
 		WebTarget targetUpdate = client.target(PATIENT_SERVICES_URL).path(PATIENTS);
 		Response response = targetUpdate.request().put(Entity.entity(patient, MediaType.APPLICATION_XML));
 		System.out.println(response.getStatus());
+		response.close();
+		client.close();
 	}
 
 	//Add patient
-	private static void insertPatient(Patient patient) 
+	private static Patient insertPatient(Patient patient) 
 	{
 		Client client = ClientBuilder.newClient();
 		WebTarget targetUpdate = client.target(PATIENT_SERVICES_URL).path(PATIENTS);
 		Patient newPatient = targetUpdate.request().post(Entity.entity(patient, MediaType.APPLICATION_XML), Patient.class);
 		System.out.println(newPatient);
+		client.close();
+		return newPatient;
+	}
+
+	private static void deletePatient(Long id) 
+	{
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(PATIENT_SERVICES_URL).path(PATIENTS).path("/{id}");
+		webTarget = webTarget.resolveTemplate("id", id);
+		Response response = webTarget.request().delete();
+		System.out.println("Status code: " + response.getStatus());
+		client.close();
 	}
 
 	//One patient
@@ -62,6 +78,7 @@ public class PatientWSClient {
 		Builder request = webTarget.request();
 		Patient patient =  request.get(Patient.class);
 		System.out.println(patient);
+		client.close();
 		return patient;
 	}
 
@@ -73,6 +90,7 @@ public class PatientWSClient {
 		Builder requestAllPatients = targetAllPatients.request();
 		List<Patient> patients =  Arrays.asList(requestAllPatients.get(Patient[].class));
 		System.out.println(patients);
+		client.close();
 		return patients;
 	}
 
